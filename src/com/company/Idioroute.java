02bca72ea367ee8bb0dde827264068e29d24bc23
vsteenbr <73848892+vsteenbr@users.noreply.github.com>
2021-09-29 18:39:01 -0400

    Generate vehicule fuction

--- src/com/company/Idioroute.java
+++ src/com/company/Idioroute.java
@@ -2,7 +2,10 @@ package com.company;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
+
 
 public class Idioroute {
     private List<Autoroute> l_autoroute;
@@ -18,13 +21,35 @@ public class Idioroute {
         l_autoroute.get(4).getL_vehicule().add(v);
     }
 
-    public void move_car(int i, Autoroute autoroute_a, Autoroute autoroute_b)
+    public void generate_vehicule()
+    {
+        Random random = new Random();
+        int random_value = random.nextInt(1+3) + 1;
+        Vehicule v;
+        if (random_value == 1)
+        {
+            v = new Voiture();
+        }
+        if (random_value == 2)
+        {
+            v = new Moto();
+        }
+        else {
+
+            v = new Camion();
+        }
+
+        l_autoroute.get(0).getL_vehicule().add(v);
+
+    }
+
+    public void change_autoroute_car(int i, Autoroute autoroute_a, Autoroute autoroute_b)
     {
         Vehicule v_to_move = autoroute_a.getL_vehicule().get(i);
-        /*if(v_to_move.getPosition() == autoroute_a.getL_acces().get(autoroute_a.getNb_acces()).getEmplacement())
+        if(v_to_move.getPosition() == autoroute_a.getL_acces().get(autoroute_a.getNb_acces()).getEmplacement())
         {
             autoroute_a.getL_vehicule().remove(v_to_move);
             autoroute_b.getL_vehicule().add(v_to_move);
-        }*/
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Vehicule voiture1 = new Voiture();
-        Vehicule voiture2 = new Moto();
-        System.out.println(voiture1.toString());
-        System.out.println(voiture2.toString());
+
+
 
 
     }
