053ab7579082c41fcd07b7fae991c791244deb7e
vsteenbr <73848892+vsteenbr@users.noreply.github.com>
2021-09-28 20:24:09 -0400

    Creation squelette différents véhicules

--- src/com/company/Voiture.java
+++ src/com/company/Voiture.java
@@ -1,4 +1,20 @@
 package com.company;
 
-public class Voiture {
+public class Voiture extends Vehicule {
+
+    public Voiture()
+    {
+        super();
+        this.vitesse = 100;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Voiture{" +
+                "vitesse=" + vitesse +
+                ", idAutoroute=" + idAutoroute +
+                ", idVehicule=" + idVehicule +
+                ", position =" + position;
+    }
 }
